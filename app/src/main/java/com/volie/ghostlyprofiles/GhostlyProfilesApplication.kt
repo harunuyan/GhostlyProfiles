3d56370c8a53c9deea2302a98dd31f9c20b05183
harun <harunuyan6@gmail.com>
2023-07-17 17:13:29 +0300

    Implemented hilt application

---
+++ app/src/main/java/com/volie/ghostlyprofiles/GhostlyProfilesApplication.kt
@@ -0,0 +1,7 @@
+package com.volie.ghostlyprofiles
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class GhostlyProfilesApplication : Application()
--- app/src/main/java/com/volie/ghostlyprofiles/ui/MainActivity.kt
+++ app/src/main/java/com/volie/ghostlyprofiles/ui/MainActivity.kt
@@ -1,8 +1,11 @@
-package com.volie.ghostlyprofiles
+package com.volie.ghostlyprofiles.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.volie.ghostlyprofiles.R
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
