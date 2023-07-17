4be4691fd45f935ba97d8c7a1e5ea4db2e59dc84
harun <harunuyan6@gmail.com>
2023-07-17 17:22:42 +0300

    Added navigation graph

--- app/src/main/java/com/volie/ghostlyprofiles/ui/MainActivity.kt
+++ app/src/main/java/com/volie/ghostlyprofiles/ui/MainActivity.kt
@@ -1,14 +1,28 @@
 package com.volie.ghostlyprofiles.ui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
 import com.volie.ghostlyprofiles.R
+import com.volie.ghostlyprofiles.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+    private var _mBinding: ActivityMainBinding? = null
+    private val mBinding get() = _mBinding!!
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        _mBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(mBinding.root)
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _mBinding = null
     }
 }
