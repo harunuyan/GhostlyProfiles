a40454ecc4bd04c60ea05e3eb5bbf7e193233f8d
harun <harunuyan6@gmail.com>
2023-07-19 23:06:39 +0300

    Added login data page fragment

---
+++ app/src/main/java/com/volie/ghostlyprofiles/ui/fragment/user_detail/page/LoginDataPageFragment.kt
@@ -0,0 +1,29 @@
+package com.volie.ghostlyprofiles.ui.fragment.user_detail.page
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.volie.ghostlyprofiles.databinding.FragmentLoginDataBinding
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class LoginDataPageFragment : Fragment() {
+    private var _mBinding: FragmentLoginDataBinding? = null
+    private val mBinding get() = _mBinding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _mBinding = FragmentLoginDataBinding.inflate(inflater, container, false)
+        return mBinding.root
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _mBinding = null
+    }
+}
