63f793b67baae2e5205c30876c6b3ead25e2bada
harun <harunuyan6@gmail.com>
2023-07-19 22:41:18 +0300

    Added personal data page fragment

---
+++ app/src/main/java/com/volie/ghostlyprofiles/ui/fragment/user_detail/page/PersonalDataPageFragment.kt
@@ -0,0 +1,29 @@
+package com.volie.ghostlyprofiles.ui.fragment.user_detail.page
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.volie.ghostlyprofiles.databinding.FragmentPersonelDataBinding
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class PersonalDataPageFragment : Fragment() {
+    private var _mBinding: FragmentPersonelDataBinding? = null
+    private val mBinding get() = _mBinding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _mBinding = FragmentPersonelDataBinding.inflate(inflater, container, false)
+        return mBinding.root
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _mBinding = null
+    }
+}
