8e70c0b553ef0acc8ac796deecd52d1ee767facf
harun <harunuyan6@gmail.com>
2023-07-26 13:58:30 +0300

    Added base view pager2 adapter

---
+++ app/src/main/java/com/volie/ghostlyprofiles/ui/adapter/BaseViewPagerAdapter.kt
@@ -0,0 +1,14 @@
+package com.volie.ghostlyprofiles.ui.adapter
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.viewpager2.adapter.FragmentStateAdapter
+
+class BaseViewPagerAdapter(
+    private val pages: List<Fragment>,
+    fragmentActivity: FragmentActivity
+) : FragmentStateAdapter(fragmentActivity) {
+    override fun getItemCount(): Int = pages.size
+
+    override fun createFragment(position: Int): Fragment = pages[position]
+}
